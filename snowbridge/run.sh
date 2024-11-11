ad2154f08fa7986a3e49851ebb7ada05e8c2b910
Paulo Jeronimo <paulo@oso.sh>
2024-11-11 12:01:38 +0000

    Refactored snowbridge/run.sh to generate less log

--- snowbridge/run.sh
+++ snowbridge/run.sh
@@ -3,11 +3,14 @@ set -eou pipefail
 cd $(dirname $0)
 BASE_DIR=$PWD
 
-SNOWBRIDGE_PATH=${SNOWBRIGE_PATH:-/opt/snowplow}
-SNOWBRIDGE_CONFIG=${SNOWBRIDGE_CONFIG:-}
+# You may want to configure this variable in a compose.yaml file:
 PROCESSOR_SLEEP=${PROCESSOR_SLEEP:-10}
-DATA_DIR=${DATA_DIR:-$BASE_DIR/data}
 
+# You can maybe configure these variables. But, you will need to adjust the Dockerfile:
+DATA_DIR=${DATA_DIR:-$BASE_DIR/data}
+SNOWBRIDGE_PATH=${SNOWBRIGE_PATH:-/opt/snowplow}
+
+# The following are internal variables. You can configure it only here:
 SNOWBRIDGE_CSV=$DATA_DIR/snowbridge.csv
 SNOWBRIDGE_LOG=$DATA_DIR/snowbridge.log
 MSG_SENT_FILE=$DATA_DIR/snowbridge.sent
@@ -15,9 +18,9 @@ DATA_TSV=$DATA_DIR/micro.tsv
 
 export PATH=$SNOWBRIDGE_PATH:$PATH
 
-zero_sent() { echo 0 > $MSG_SENT_FILE; }
+send_zero_to() { echo 0 > $1; }
 
-[ -f $MSG_SENT_FILE ] || zero_sent
+[ -f $MSG_SENT_FILE ] || send_zero_to $MSG_SENT_FILE
 
 while true
 do
@@ -25,7 +28,6 @@ do
     then
         timestamp=$(date +%s)
         start=$(( $(<$MSG_SENT_FILE) + 1 ))
-        echo At $timestamp: sending data from $DATA_TSV to snowbridge, starting from line $start.
         snowbridge_log=$SNOWBRIDGE_LOG.$timestamp
         {
             sed -n "$start,\$p" $DATA_TSV |
@@ -34,15 +36,15 @@ do
             grep -oP '(?<=,|^)MsgSent:\K\d+' > $MSG_SENT_FILE
         } || {
             echo -e snowbridge reported a failure: "\n$(<$snowbridge_log)"
-            zero_sent
+            send_zero_to $MSG_SENT_FILE
         }
-        echo "$(<$MSG_SENT_FILE)" messages were processed.
-        echo "$timestamp,$(< $MSG_SENT_FILE)" >> $SNOWBRIDGE_CSV
-        more_sent=$(<$MSG_SENT_FILE)
-        echo $(( start - 1 + more_sent )) > $MSG_SENT_FILE
-    else
-        echo File $DATA_TSV is not available.
+        total_sent="$(<$MSG_SENT_FILE)"
+        if [ $total_sent -gt 0 ]
+        then
+            echo At $timestamp, $total_sent messages were processed from $DATA_TSV starting from line $start.
+            echo "$timestamp,$total_sent" >> $SNOWBRIDGE_CSV
+            echo $(( start - 1 + total_sent )) > $MSG_SENT_FILE
+        fi
     fi
-    echo Waiting $PROCESSOR_SLEEP seconds to run snowbridge again ...
     sleep $PROCESSOR_SLEEP
 done

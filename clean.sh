59ddeadf6d50f21d8e683baab80b2dd71f000335
Paulo Jeronimo <paulo@oso.sh>
2024-11-17 10:24:13 +0000

    Made a call to docker volume proune in clean.sh

--- clean.sh
+++ clean.sh
@@ -8,11 +8,13 @@ echo NOTE: You are expected to run this script with containers not running.
 echo Removing containers ...
 docker compose rm --volumes || :
 
+echo Removing volumes ...
 for volume in zookeeper_data zookeeper_logs kafka_data kafka_logs
 do
     echo Removing volume ${BASE_DIR}_$volume ...
     docker volume rm ${BASE_DIR}_$volume || :
 done
+yes | docker volume prune || :
 
 ./data/clean.sh all
 
